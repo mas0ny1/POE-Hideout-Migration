c49b3efc6b7145674244268684d9c634050b2be5
mas0ny1 <masonpun3@gmail.com>
2023-12-20 17:02:58 +1100

    Testing Scourge Dimenion Hideout Decoration
    Trying to figure out how to get it so that can have anime girl in main dimension, then another when you click on the scourge hideout decoration.
    
    Discoveries so far:
    fv seems to be correlated to scourge dimension (fv = 64)
    To move items into the scourge dimension, you need to first have the scourge dimension item listed (in the doodads list) before having everythinig after only being items in the scourge dimension (everything must have an fv of 64). The order of an item is tacked onto the end of the file (so the next decoration you add gets placed at the end of the file, so it seems like order matters for the scourge dimension)
    
    My next idea is to test to see if fv is the layering or exposure or visibility value or something by changing it and seeing if it changes the image

--- move.py
+++ move.py
@@ -1,20 +1,27 @@
+import json
+
 #Changes all objects X and Y coordinates by xRelative, YRelative 
 #Credits to https://gist.github.com/CristenPerret/ea3da944c2e976408662b988ee07d9e6
 
 #Edit values here
-fileName = 'HIDEOUT FILE NAME HERE (Filename.hideout)'
+fileName = 'WhiteEditFV.hideout'
 xRelative = 0
 yRelative = 0
-fileName_output = 'OUTPUT FILE NAME HERE (FilenameOutput.hideout)' 
+fvFixed = 64 #64 after Scourge Hideout Decoration (fv: 64) is how you add decorations to the scourge dimension
+fileName_output = 'test.hideout' 
 
 #If changing hideout 'tiles/map' you must change the "hideout_name" + "hideout_hash" manually.
 #- IE Moving from backstreet to shaped. Open a shaped hideout file. 
 #- Copy the name and hash line (line 4+5), then replace those lines in your backstreet file.
 
+
+
+
 #errors="ignore": used to ignore unicode errors that can occur when reading a hideoutfile that was made in a different language
 with open(fileName, errors="ignore") as f:
     lines = f.readlines()
     lines = [line.strip('\n') for line in lines]
+
 
 output_file = []
 for line in lines:
@@ -27,6 +34,11 @@ for line in lines:
         coord = int(line.split(':')[1].rstrip(','))
         coord += yRelative
         line = '          "y":' + str(coord) + ","
+        output_file.append(line + '\n') 
+    elif '"fv":' in line:
+        coord = int(line.split(':')[1].rstrip(','))
+        coord = fvFixed
+        line = '      "fv":' + str(coord) + ","
         output_file.append(line + '\n')
         pass
     else:
